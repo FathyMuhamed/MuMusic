90f69fb8a8f94de6f9fbe4674ee6cc72fd1ffde5
FathyMuhamed <fathimuhamed10522@gmail.com>
2022-10-08 23:52:27 +0200

    add chakraUi config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,40 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#F5f5f5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
